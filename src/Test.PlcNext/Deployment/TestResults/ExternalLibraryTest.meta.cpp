198afa344a46c849c9b24a971f4c016652a85bd9
twilker <twilker@phoenixcontact.com>
2019-11-22 08:31:07 +0100

    Revert multiple namespaces for port structures. #exported-commit:f3fe73664cb2c700788d379dc20111618192ae58

--- src/Test.PlcNext/Deployment/TestResults/ExternalLibraryTest.meta.cpp
+++ src/Test.PlcNext/Deployment/TestResults/ExternalLibraryTest.meta.cpp
@@ -9,9 +9,9 @@ namespace ExternalLibraryTest
 
 using namespace Arp::Plc::Commons::Meta;
 
-    static const FieldDefinition My_Company_Name_ExternalStruct_Member1("Member1", offsetof(::My::Company::Name::ExternalStruct, Member1), DataType::Int64, "", sizeof(int64), alignof(int64), {  }, StandardAttribute::None);
-    static const FieldDefinition My_Company_Name_ExternalStruct_Member2("Member2", offsetof(::My::Company::Name::ExternalStruct, Member2), DataType::Int16, "", sizeof(int16), alignof(int16), {  }, StandardAttribute::None);
-    static const FieldDefinition ExternalLibraryTest_ExternalLibraryTestProgram_port("ExternalPort", offsetof(::ExternalLibraryTest::ExternalLibraryTestProgram, port), DataType::Struct, CTN<My::Company::Name::ExternalStruct>(), sizeof(My::Company::Name::ExternalStruct), alignof(My::Company::Name::ExternalStruct), {  }, StandardAttribute::Output);
+    static const FieldDefinition ExternalLibraryTest_ExternalStruct_Member1("Member1", offsetof(::ExternalLibraryTest::ExternalStruct, Member1), DataType::Int64, "", sizeof(int64), alignof(int64), {  }, StandardAttribute::None);
+    static const FieldDefinition ExternalLibraryTest_ExternalStruct_Member2("Member2", offsetof(::ExternalLibraryTest::ExternalStruct, Member2), DataType::Int16, "", sizeof(int16), alignof(int16), {  }, StandardAttribute::None);
+    static const FieldDefinition ExternalLibraryTest_ExternalLibraryTestProgram_port("ExternalPort", offsetof(::ExternalLibraryTest::ExternalLibraryTestProgram, port), DataType::Struct, CTN<ExternalLibraryTest::ExternalStruct>(), sizeof(ExternalLibraryTest::ExternalStruct), alignof(ExternalLibraryTest::ExternalStruct), {  }, StandardAttribute::Output);
     
     void ExternalLibraryTestLibrary::InitializeTypeDomain()
     {
@@ -19,12 +19,12 @@ using namespace Arp::Plc::Commons::Meta;
         (
             // Begin TypeDefinitions
             {
-                {   // TypeDefinition: My::Company::Name::ExternalStruct
-                    DataType::Struct, CTN<My::Company::Name::ExternalStruct>(), sizeof(::My::Company::Name::ExternalStruct), alignof(::My::Company::Name::ExternalStruct), StandardAttribute::None,
+                {   // TypeDefinition: ExternalLibraryTest::ExternalStruct
+                    DataType::Struct, CTN<ExternalLibraryTest::ExternalStruct>(), sizeof(::ExternalLibraryTest::ExternalStruct), alignof(::ExternalLibraryTest::ExternalStruct), StandardAttribute::None,
                     {
                         // FieldDefinitions:
-                        My_Company_Name_ExternalStruct_Member1,
-                        My_Company_Name_ExternalStruct_Member2,
+                        ExternalLibraryTest_ExternalStruct_Member1,
+                        ExternalLibraryTest_ExternalStruct_Member2,
                     }
                 },
                 {   // ProgramDefinition: ExternalLibraryTest::ExternalLibraryTestProgram
