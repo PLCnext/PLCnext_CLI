b912b158fc7e36e2d29c766eabb2da126833fcf8
twilker <twilker@phoenixcontact.com>
2020-06-08 07:34:58 +0200

    Github synchronizerAdd github synchronizer with script. It is disabled as the proxy is interfering with large pushes. When that is resolved the implementation is continued. #exported-commit:ca53441ce8146a558b42d9f8e22567cc3165983a

--- src/PlcNext.CppParser/CppRipper/CppStructuralGrammar.cs
+++ src/PlcNext.CppParser/CppRipper/CppStructuralGrammar.cs
@@ -1,13 +1,4 @@
-﻿#region Copyright
-///////////////////////////////////////////////////////////////////////////////
-//
-//  Copyright (c) Phoenix Contact GmbH & Co KG
-//  This software is licensed under Apache-2.0
-//
-///////////////////////////////////////////////////////////////////////////////
-#endregion
-
-using System.Reflection;
+﻿using System.Reflection;
 
 namespace PlcNext.CppParser.CppRipper
 {
