98d72d2599f4d9eea4b94daeea5b25f766e5b670
pxcsw-afbuild <67624542+pxcsw-afbuild@users.noreply.github.com>
2022-07-29 14:57:51 +0200

    format error to avoid formatting problem in VS #exported-commit:0ed9b1d8cbb9f0d1b68970f033c5574333241f37

--- src/PlcNext.Common/CodeErrors.Designer.cs
+++ src/PlcNext.Common/CodeErrors.Designer.cs
@@ -28,7 +28,7 @@ namespace PlcNext.Common {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class CodeErrors {
@@ -70,7 +70,8 @@ namespace PlcNext.Common {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to The restriction for the values of the attribute &apos;{0}&apos; does not allow the value &apos;{1}&apos;. {2}.
+        ///   Looks up a localized string similar to The restriction for the values of the attribute &apos;{0}&apos; does not allow the value &apos;{1}&apos;.
+        ///{2}.
         /// </summary>
         internal static string ARP0001 {
             get {
